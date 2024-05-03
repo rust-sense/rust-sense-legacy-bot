163561c6910ddda515f07824041ee23d0eab07e8
Jaime Martínez Rincón <jaime@jamezrin.name>
2024-05-04 01:07:14 +0200

    fix: smart switch

--- src/util/FcmListenerLite.js
+++ src/util/FcmListenerLite.js
@@ -77,7 +77,8 @@ module.exports = async (client, guild, steamId) => {
 
                         case 'entity': {
                             switch (body.entityName) {
-                                case 'Switch': {
+                                case 'Switch':
+                                case 'Smart Switch': {
                                     client.log('FCM LITE',
                                         `GuildID: ${guild.id}, SteamID: ${steamId}, pairing: entity: Switch`);
                                     pairingEntitySwitch(client, guild, full, data, body);
