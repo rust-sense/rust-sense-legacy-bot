e6680f1a609cf92543cbf8feb0fc918889523beb
Jaime Martínez Rincón <jaime@jamezrin.name>
2025-02-01 00:45:27 +0100

    chore: add debug info

--- src/utils/playerNameUtils.ts
+++ src/utils/playerNameUtils.ts
@@ -2,6 +2,7 @@ import { PlayerInfo, PlayerNameType } from '../types/Player';
 import { getStreamerModeUsername } from './streamerModeUtils';
 
 export function getPlayerName(playerNameType: PlayerNameType, playerInfo: PlayerInfo): string {
+    console.log('getPlayerName', playerNameType, playerInfo);
     switch (playerNameType) {
         case 'realName':
             return playerInfo.name;
