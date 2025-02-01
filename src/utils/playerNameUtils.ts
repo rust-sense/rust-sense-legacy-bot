30c03cd79385117a10a2615636826b840197a360
Jaime Martínez Rincón <jaime@jamezrin.name>
2025-02-01 19:04:01 +0100

    chore: delete log in getPlayerName

--- src/utils/playerNameUtils.ts
+++ src/utils/playerNameUtils.ts
@@ -2,7 +2,6 @@ import { PlayerInfo, PlayerNameType } from '../types/Player';
 import { getStreamerModeUsername } from './streamerModeUtils';
 
 export function getPlayerName(playerNameType: PlayerNameType, playerInfo: PlayerInfo): string {
-    console.log('getPlayerName', playerNameType, playerInfo);
     switch (playerNameType) {
         case 'realName':
             return playerInfo.name;
