3eb5eadef92b315084f1cd356d1796ab22ac29c2
Jaime Martínez Rincón <jaime@jamezrin.name>
2025-07-04 17:43:49 +0200

    fix: format and install

--- src/discordTools/discordEmbeds.js
+++ src/discordTools/discordEmbeds.js
@@ -7,7 +7,10 @@ const InstanceUtils = require('../util/instanceUtils');
 const Timer = require('../util/timer');
 
 function isValidUrl(url) {
-    if (url.startsWith('https') || url.startsWith('http')) return true;
+    if (url.startsWith('https') || url.startsWith('http')) {
+        return true;
+    }
+
     return false;
 }
 
@@ -603,7 +606,7 @@ module.exports = {
             footer: { text: body.name },
             title: data.title,
             description: data.message,
-            thumbnail: (body.img !== '' && isValidUrl(body.img)) ? body.img : 'attachment://rocket.png'
+            thumbnail: body.img !== '' && isValidUrl(body.img) ? body.img : 'attachment://rocket.png',
         });
     },
 
